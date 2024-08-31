01fd02399986726d4f4614ba37eeb4eacf7f4439
Mikhail Yevchenko <m..@uo1.net>
2024-08-31 14:37:34 +0300

    Revert "KVS-56 Preload static thumbs on initialization"
    This reverts commit bd2c9c0139903339e10add4d48597a267339d41e.

--- src/modules/timeline.js
+++ src/modules/timeline.js
@@ -193,15 +193,6 @@ export default function (playerInstance, options) {
         } else if ('static' === timelinePreview.type && typeof timelinePreview.frames === 'object') {
             timelinePreview.spriteImage = true;
             playerInstance.timelinePreviewData = timelinePreview.frames;
-
-            let images = {};
-
-            playerInstance.timelinePreviewData.forEach(frame => {
-                if (typeof images[frame.image] === 'undefined') {
-                    images[frame.image] = document.createElement('img');
-                    images[frame.image].src = frame.image;
-                }
-            });
         } else {
             throw 'Invalid thumbnail-preview - type must be VTT or static';
         }
